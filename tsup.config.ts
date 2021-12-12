ef7534f0fdba9b6cedf3b0e1b88e3b51aba48737
aslemammad <zorofight94@gmail.com>
2021-12-12 20:09:09 +0330

    build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entryPoints: ['src/index.ts'],
+  entryPoints: [
+    'src/index.ts',
+    'src/spy.ts',
+    'src/spyOn.ts',
+    'src/restoreAll.ts',
+  ],
   outDir: 'dist',
   format: ['cjs', 'esm'],
   external: ['jotai', '@apollo/client', 'wonka'],
   tsconfig: './tsconfig.json',
   target: 'es2017',
   splitting: false,
-  sourcemap: true,
   clean: true,
   dts: true,
 })
